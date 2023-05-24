e3b7c11b374cd097702495048d4147854e86c18d
RobertoStagg <stagg.roberto@gmail.com>
2023-05-24 12:15:02 -0700

    Order of operations function in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
 
   numberPressed(integer: number){
     this.expression.push(integer)
-    console.log("Expression: " + this.expression)
+    //console.log("Expression: " + this.expression)
     //console.log("Number pressed: " + integer)
   }
   operatorPressed(operator: string){
@@ -69,8 +69,8 @@ function calculateResult(expression: any[]) {
           highest_operator.operator = divide;
           highest_operator.index = i;
         }
-          console.log("divide found, index: ", [i])
-          console.log("highest operator: ", highest_operator)
+          //console.log("divide found, index: ", [i])
+          //console.log("highest operator: ", highest_operator)
           break;
       }
       case '*': {
@@ -84,8 +84,8 @@ function calculateResult(expression: any[]) {
           highest_operator.operator = multiply;
           highest_operator.index = i;
         }
-          console.log("multiply found, index: ", [i])
-          console.log("highest operator: ", highest_operator)
+          //console.log("multiply found, index: ", [i])
+          //console.log("highest operator: ", highest_operator)
           break;
       }
       case '+': {
@@ -99,8 +99,8 @@ function calculateResult(expression: any[]) {
           highest_operator.operator = add;
           highest_operator.index = i;
         }
-          console.log("add found, index: ", [i])
-          console.log("highest operator: ", highest_operator)
+          //console.log("add found, index: ", [i])
+          //console.log("highest operator: ", highest_operator)
           break;
       }
       case '-': {
@@ -114,8 +114,8 @@ function calculateResult(expression: any[]) {
           highest_operator.operator = subtract;
           highest_operator.index = i;
         }
-          console.log("subtract found, index: ", [i])
-          console.log("operator name: ", highest_operator)
+          //console.log("subtract found, index: ", [i])
+          //console.log("operator name: ", highest_operator)
           break;
       }
       default: {
@@ -123,8 +123,44 @@ function calculateResult(expression: any[]) {
       }
     }
     }
-  //capture the adjacent numbers
-  
+
+  //capture the number to the left of the operator
+  for (let i = highest_operator.index - 1; i > 0; i--){ 
+    console.log("i: ", expression[i], "index: ", i)
+    // Start to the left of the highest operator and work backwards
+    // Stop when the next operator is found or the end of the array is reached
+    if (!isNaN(expression[i])){
+      //console.log("Number found," + expression[i])
+    }
+    else if (isNaN(expression[i])){
+      //console.log("Operator, ", expression[i])
+      var left_operand = expression.slice(expression[i+1], highest_operator.index)
+      console.log("Left operand: ", left_operand.join(''))
+      break
+    }
+    else if (i == 0){
+      var left_operand = expression.slice(0, highest_operator.index)
+      console.log("Left operand: ", left_operand.join(''))
+      break
+    }
+  }
+  if (highest_operator.index -1 == 0 && !isNaN(expression[0])){
+    console.log("The left operand is the first number in the expression: ", expression[0])
+  }
+
+  //capture the number to the right of the operator
+  for (let i = highest_operator.index + 1; i < expression.length; i++){
+    if (!isNaN(expression[i])){
+      //console.log("Number found," + expression[i])
+    }
+    else if (isNaN(expression[i])){
+      //console.log("Operator, ", expression[i])
+      var right_operand = expression.slice(highest_operator.index + 1, i)
+      console.log("Right operand: ", right_operand.join(''))
+      break
+    }
+  }
+
   //calculate adjacent numbers with highest operator them and replacing the result 
 
   }
