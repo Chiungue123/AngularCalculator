b80bd25ebfe640aa7cf0a3fa6fa2b7462116d849
RobertoStagg <stagg.roberto@gmail.com>
2023-05-26 08:33:51 -0700

    Can handle multiple operators and decimals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   operator!: string; // Variable to store the operator
   number!: number; // Variable to store the number
   isResultShown: boolean = false;
+  finalResult!: number;
 
   numberPressed(integer: number){
     this.expression.push(integer)
@@ -24,8 +25,8 @@ export class AppComponent {
     switch(operator){ 
       case '=': {
         console.log("Calculating result")
-        this.expression.push(" ",operator)
-        this.result = calculateResult(this.expression)
+        this.expression.push(" = ")
+        this.finalResult = calculateResult(this.expression)
         this.isResultShown = true
         break
       }
@@ -45,145 +46,129 @@ export class AppComponent {
 
 function calculateResult(expression: any[]) {
   // TODO: Calculate the result of the expression
+  let simplified = false; // This will be used to check if the expression has been simplified
+  let result: number = 0; // This will store the result of the expression
+  let finalResult: number = 0; // This will store the final result of the expression
 
-  var highest_operator = { // This will keep track of the highest operator
-    name: '',
-    operator: 0,
-    index: -1
-  } 
+  while (!simplified){
+    var highest_operator = { // This will keep track of the highest operator
+      name: '',
+      operator: 0,
+      index: -1
+    } 
 
-  let result!: number; // This will store the result of the expression
+    // Sets the operator values
+    const bracket: number = 1;
+    const exponent: number = 2;
+    const divide: number = 3;
+    const multiply: number = 4;
+    const add: number = 5;
+    const subtract: number = 6;
 
-  // Sets the operator values
-  const bracket: number = 1;
-  const exponent: number = 2;
-  const divide: number = 3;
-  const multiply: number = 4;
-  const add: number = 5;
-  const subtract: number = 6;
+    // Sets the left and right operand variables
+    var left_operand: any[] = [];
+    var right_operand: any[] = [];
 
-  // Sets the left and right operand variables
-  var left_operand: any[] = [];
-  var right_operand: any[] = [];
-
-  // Record highest order operater first
-  for (let i = 0; i < expression.length; i++){
-    switch (expression[i]){
-      case '/': {
-        if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
-          highest_operator.name = 'divide';
-          highest_operator.operator = divide;
-          highest_operator.index = i;
+    // Record highest order operater first
+    for (let i = 0; i < expression.length; i++){
+      switch (expression[i]){
+        case '/': {
+          if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
+            highest_operator.name = 'divide';
+            highest_operator.operator = divide;
+            highest_operator.index = i;
+          }
+          else if (highest_operator.operator > divide){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
+            highest_operator.name = 'divide';
+            highest_operator.operator = divide;
+            highest_operator.index = i;
+          }
+            //console.log("divide found, index: ", [i])
+            //console.log("highest operator: ", highest_operator)
+            break;
         }
-        else if (highest_operator.operator > divide){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
-          highest_operator.name = 'divide';
-          highest_operator.operator = divide;
-          highest_operator.index = i;
+        case '*': {
+          if (highest_operator.operator == 0){ // Set multiply as highest operator if the highest_operator is null
+            highest_operator.name = 'multiply';
+            highest_operator.operator = multiply;
+            highest_operator.index = i;
+          }
+          else if (highest_operator.operator > multiply){ // Compare current highest operator value to the multiply operator value, lowest number becomes the highest operator
+            highest_operator.name = 'multiply';
+            highest_operator.operator = multiply;
+            highest_operator.index = i;
+          }
+            //console.log("multiply found, index: ", [i])
+            //console.log("highest operator: ", highest_operator)
+            break;
         }
-          //console.log("divide found, index: ", [i])
-          //console.log("highest operator: ", highest_operator)
-          break;
+        case '+': {
+          if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
+            highest_operator.name = 'add';
+            highest_operator.operator = add;
+            highest_operator.index = i;
+          }
+          else if (highest_operator.operator > add){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
+            highest_operator.name = 'add';
+            highest_operator.operator = add;
+            highest_operator.index = i;
+          }
+            //console.log("add found, index: ", [i])
+            //console.log("highest operator: ", highest_operator)
+            break;
+        }
+        case '-': {
+          if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
+            highest_operator.name = 'subtract';
+            highest_operator.operator = subtract;
+            highest_operator.index = i;
+          }
+          else if (highest_operator.operator > subtract){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
+            highest_operator.name = 'subtract';
+            highest_operator.operator = subtract;
+            highest_operator.index = i;
+          }
+            //console.log("subtract found, index: ", [i])
+            //console.log("operator name: ", highest_operator)
+            break;
+        }
+        default: {
+          continue
+        }
       }
-      case '*': {
-        if (highest_operator.operator == 0){ // Set multiply as highest operator if the highest_operator is null
-          highest_operator.name = 'multiply';
-          highest_operator.operator = multiply;
-          highest_operator.index = i;
-        }
-        else if (highest_operator.operator > multiply){ // Compare current highest operator value to the multiply operator value, lowest number becomes the highest operator
-          highest_operator.name = 'multiply';
-          highest_operator.operator = multiply;
-          highest_operator.index = i;
-        }
-          //console.log("multiply found, index: ", [i])
-          //console.log("highest operator: ", highest_operator)
-          break;
       }
-      case '+': {
-        if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
-          highest_operator.name = 'add';
-          highest_operator.operator = add;
-          highest_operator.index = i;
-        }
-        else if (highest_operator.operator > add){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
-          highest_operator.name = 'add';
-          highest_operator.operator = add;
-          highest_operator.index = i;
-        }
-          //console.log("add found, index: ", [i])
-          //console.log("highest operator: ", highest_operator)
-          break;
-      }
-      case '-': {
-        if (highest_operator.operator == 0){ // Set divide as highest operator if the highest_operator is null
-          highest_operator.name = 'subtract';
-          highest_operator.operator = subtract;
-          highest_operator.index = i;
-        }
-        else if (highest_operator.operator > subtract){ // Compare current highest operator value to the divide operator value, lowest number becomes the highest operator
-          highest_operator.name = 'subtract';
-          highest_operator.operator = subtract;
-          highest_operator.index = i;
-        }
-          //console.log("subtract found, index: ", [i])
-          //console.log("operator name: ", highest_operator)
-          break;
-      }
-      default: {
-        continue
-      }
-    }
-    }
 
-  //capture the number to the left of the operator
-  if (highest_operator.index -1 == 0 && !isNaN(expression[0])){
-    left_operand = [expression[0]]
-    console.log("Left operand: ", left_operand)
-  }
-  else {
+    //capture the number to the left of the operator
     for (let i = highest_operator.index - 1; i >= 0; i--){ 
 
-      if (!isNaN(expression[i])){
-        console.log("Number found," + expression[i])
-        if (i == 0 && !isNaN(expression[0])){
-          left_operand = expression.slice(0, highest_operator.index)
-          console.log("Left operand: ", left_operand.join(''))
-          break
-        }
+      // If a number or decimal is found, display it and continue iterating backwards
+      if (!isNaN(expression[i]) || expression[i] == '.'){ 
+        left_operand.unshift(expression[i])
       }
-
-      else if (isNaN(expression[i])){
-        console.log("Operator found: ", expression[i])
-        left_operand = expression.slice(expression[i+1], highest_operator.index)
+      
+      // If an operator is found, stop iterating
+      else if (['/', '*', '+', '-'].includes(expression[i])){
         console.log("Left operand: ", left_operand.join(''))
         break
       }
     }
-  }
 
-  //capture the number to the right of the operator
-  if (highest_operator.index + 1 == (expression.length - 1) && !isNaN(expression[expression.length - 1])){
-    right_operand = [expression[expression.length - 1]]
-    console.log("Right opperand: ", right_operand)
-  }
-  else {
-    for (let i = highest_operator.index + 1; i <= expression.length; i++){
-      if (!isNaN(expression[i])){
-        console.log("Number found," + expression[i])
-        if (i == expression.length - 1 && !isNaN(expression[expression.length - 1])){
-          right_operand = expression.slice(highest_operator.index + 1, expression.length)
-          console.log("Right operand: ", right_operand.join(''))
-          break
-        }
+    //capture the number to the right of the operator
+    for (let i = highest_operator.index + 1; i < expression.length; i++){
+
+      if (!isNaN(expression[i]) || expression[i] == '.'){
+        right_operand.push(expression[i]) 
       }
 
-      else if (isNaN(expression[i])){
-        right_operand = expression.slice(highest_operator.index + 1, i)
-        console.log("Right operand: ", right_operand.join(''))
+      else if (['/', '*', '+', '-'].includes(expression[i])){
+        // If an operator is found, stop iterating
+        //console.log("Right operand: ", right_operand.join(''))
         break
       }
     }
-  }
+
+  console.log("Left operand after loop: ", left_operand.join(''));
+  console.log("Right operand after loop: ", right_operand.join(''));
 
   //calculate adjacent numbers with highest operator them and replacing the result 
   switch (highest_operator.name){
@@ -212,7 +197,19 @@ function calculateResult(expression: any[]) {
       console.log("Result: ", result)
       break
   }
-}
-return result;
+  }
+  // Simplify the expression
+  expression.splice(highest_operator.index - left_operand.length, left_operand.length + right_operand.length + 1, result)
+  console.log("Expression after splice: ", expression.join(''))
+
+  if (!expression.some(char => ['/', '*', '+', '-'].includes(char))) { 
+    // If there are no more operators in the expression, the expression is simplified
+    simplified = true;
+    console.log("Expression simplified!")
+    break;
+  }
+  }
+  finalResult = result
+  return finalResult
 }
   // TODO: Display the result on the calculator screen
